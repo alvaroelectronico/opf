61029c1ba04171d4f07b1053380da620ba6a2d9d
lauramardones123 <laura.mardones@alumnos.upm.es>
2025-07-12 10:26:51 +0200

    Update cargarFunciones_PSO.jl

--- PSO/cargarFunciones_PSO.jl
+++ PSO/cargarFunciones_PSO.jl
@@ -8,12 +8,12 @@ include("./gestorResultados_PSO.jl")            # Imprimir los resultados del AC
 
 include("../Funciones/limpiarTerminal.jl")      # Limpiar la terminal
 
-include("./pso_hibrido.jl")                     # Cargar todas las funciones relacionadas con el PSO híbrido
-include("./calcularTensiones.jl")               # Cargar función de cálculo de tensiones con Newton Raphson
-include("./calcularFlujos.jl")                  # Cargar función de cálculo de flujos
-include("./calcularAdmitancias.jl")             # Cargar el cálculo de la matriz de admitancias
+#include("./pso_hibrido.jl")                     # Cargar todas las funciones relacionadas con el PSO híbrido
+#include("./calcularTensiones.jl")               # Cargar función de cálculo de tensiones con Newton Raphson
+#include("./calcularFlujos.jl")                  # Cargar función de cálculo de flujos
+#include("./calcularAdmitancias.jl")             # Cargar el cálculo de la matriz de admitancias
 
-#include("./pso_hibrido_lineas.jl")              # Cargar todas las funciones relacionadas con el PSO de cambio de topología
-#include("./calcularTensiones_lineas.jl")        # Cargar función de cálculo de tensiones teniendo en cuenta el estado de las líneas
-#include("./calcularFlujos_lineas.jl")           # Cargar función de cálculo de flujos teniendo en cuenta el estado de las líneas
-#include("./calcularAdmitancias_lineas.jl")      # Cargar función de cálculo de admitancias con líneas abiertas
+include("./pso_hibrido_lineas.jl")              # Cargar todas las funciones relacionadas con el PSO de cambio de topología
+include("./calcularTensiones_lineas.jl")        # Cargar función de cálculo de tensiones teniendo en cuenta el estado de las líneas
+include("./calcularFlujos_lineas.jl")           # Cargar función de cálculo de flujos teniendo en cuenta el estado de las líneas
+include("./calcularAdmitancias_lineas.jl")      # Cargar función de cálculo de admitancias con líneas abiertas
